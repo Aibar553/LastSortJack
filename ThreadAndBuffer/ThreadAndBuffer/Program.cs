ffe9e33b6b6ffa3d844f6b679944bad4dca7cd6a
Aibar553 <74353064+Aibar553@users.noreply.github.com>
2023-11-27 21:03:26 +0600

    Add files via upload

---
+++ ThreadAndBuffer/ThreadAndBuffer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class FileSearchUtility
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Использование: utility.exe <расширение файла> <текст для поиска>");
+            return;
+        }
+
+        string targetExtension = args[0];
+        string searchText = args[1];
+
+        string currentDirectory = Environment.CurrentDirectory;
+        SearchFiles(currentDirectory, targetExtension, searchText);
+
+        Console.WriteLine("Поиск завершен.");
+    }
+
+    static void SearchFiles(string directory, string targetExtension, string searchText)
+    {
+        try
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(directory, $"*.{targetExtension}");
+
+            foreach (string file in files)
+            {
+                if (FileContainsText(file, searchText))
+                {
+                    Console.WriteLine($"Найден файл: {file}");
+                }
+            }
+
+            IEnumerable<string> subdirectories = Directory.EnumerateDirectories(directory);
+
+            foreach (string subdirectory in subdirectories)
+            {
+                SearchFiles(subdirectory, targetExtension, searchText);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при поиске файлов: {ex.Message}");
+        }
+    }
+
+    static bool FileContainsText(string filePath, string searchText)
+    {
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            return content.Contains(searchText);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при чтении файла {filePath}: {ex.Message}");
+            return false;
+        }
+    }
+}
+
